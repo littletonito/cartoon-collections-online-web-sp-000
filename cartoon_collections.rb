f24f367764ee77944ea3b2649b0ef8dc8f3d491a
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-10-07 22:49:10 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+
+def roll_call_dwarves(dwarfs)# code an argument here
   # Your code here
+  dwarfs.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+
+
+def summon_captain_planet(planeteer)# code an argument here
   # Your code here
+  planeteer.map! do |name|
+    name.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer)# code an argument here
   # Your code here
+  planeteer.any? { |e| e.size > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |e|
+    e.include?("cheddar")
+
+  end
 end
